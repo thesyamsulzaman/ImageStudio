a70c4a4208b425fb22066acb94cc61f86a9a71c2
Syamsul <Syamsul@LAPTOP-PC6PP5HH>
2023-07-30 06:03:50 +0700

    Add project files.

---
+++ ImageStudio/Form1.Designer.vb
@@ -0,0 +1,82 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        pictureDisplay = New PictureBox()
+        buttonLoad = New Button()
+        openFileDialog = New OpenFileDialog()
+        convertImage = New Button()
+        CType(pictureDisplay, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' pictureDisplay
+        ' 
+        pictureDisplay.BackColor = SystemColors.ControlDark
+        pictureDisplay.BorderStyle = BorderStyle.Fixed3D
+        pictureDisplay.Location = New Point(12, 12)
+        pictureDisplay.Name = "pictureDisplay"
+        pictureDisplay.Size = New Size(645, 426)
+        pictureDisplay.TabIndex = 0
+        pictureDisplay.TabStop = False
+        ' 
+        ' buttonLoad
+        ' 
+        buttonLoad.Location = New Point(663, 12)
+        buttonLoad.Name = "buttonLoad"
+        buttonLoad.Size = New Size(125, 36)
+        buttonLoad.TabIndex = 1
+        buttonLoad.Text = "Load Image"
+        buttonLoad.UseVisualStyleBackColor = True
+        ' 
+        ' openFileDialog
+        ' 
+        openFileDialog.FileName = "openFileDialog"
+        ' 
+        ' convertImage
+        ' 
+        convertImage.Location = New Point(663, 54)
+        convertImage.Name = "convertImage"
+        convertImage.Size = New Size(125, 39)
+        convertImage.TabIndex = 2
+        convertImage.Text = "Convert Image"
+        convertImage.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(convertImage)
+        Controls.Add(buttonLoad)
+        Controls.Add(pictureDisplay)
+        Name = "Form1"
+        Text = "Form1"
+        CType(pictureDisplay, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents pictureDisplay As PictureBox
+    Friend WithEvents buttonLoad As Button
+    Friend WithEvents openFileDialog As OpenFileDialog
+    Friend WithEvents convertImage As Button
+End Class
---
+++ ImageStudio/Form1.vb
@@ -0,0 +1,78 @@
+﻿Public Class Form1
+    Dim bmp01, bmp02, bmpTemp As Bitmap
+    Dim clr, clrTemp As Color
+    Dim intR, intG, intB, intTemp As Integer
+
+    Private Sub buttonLoad_Click(sender As Object, e As EventArgs) Handles buttonLoad.Click
+        Dim intResponse As Integer
+        Dim sngMul As Single
+
+        openFileDialog.Filter = "Bitmap Files|*.bmp|JPEG Files|*.jpg|GIF Files|*.gif"
+        If openFileDialog.ShowDialog Then
+            bmp01 = New Bitmap(Image.FromFile(openFileDialog.FileName))
+
+            If bmp01.Width > 640 Or bmp01.Height > 580 Then
+                intResponse = MsgBox _
+            ("Resolusi gambar terlalu besar. mau di resize biar muat?",
+            vbYesNo + vbQuestion)
+            End If
+
+            If intResponse = vbNo Then
+                Exit Sub
+            ElseIf intResponse = vbYes Then
+                If (bmp01.Width / 4) >= (bmp01.Height / 3) Then
+                    sngMul = 1 / (bmp01.Width / 640)
+                Else
+                    sngMul = 1 / (bmp01.Height / 480)
+                End If
+                bmpTemp = New Bitmap(bmp01, (bmp01.Width * sngMul), (bmp01.Height * sngMul))
+                bmpTemp.SetResolution(bmp01.HorizontalResolution, bmp01.VerticalResolution)
+                bmp01 = bmpTemp
+            End If
+
+            bmp02 = New Bitmap(bmp01)
+            bmpTemp = New Bitmap(bmp01)
+            pictureDisplay.Image = bmp01
+            Me.Text = "ImageStudio(" & openFileDialog.FileName & ")"
+        End If
+
+
+    End Sub
+
+    Private Sub convertImage_Click(sender As Object, e As EventArgs) Handles convertImage.Click
+        If pictureDisplay.Image Is Nothing Then
+            Exit Sub
+        End If
+
+        Dim partWidth As Integer = bmp02.Width \ 3
+
+        For y As Integer = 0 To bmp02.Height - 1
+            For x As Integer = 0 To bmp02.Width - 1
+                Dim partIndex As Integer = x \ partWidth
+                clr = bmp02.GetPixel(x, y)
+
+                Dim originalColor As Color = bmp02.GetPixel(x, y)
+
+
+                Select Case partIndex
+                    Case 0
+                        clrTemp = Color.FromArgb(originalColor.A, originalColor.R, 0, 0)
+                    Case 1
+                        clrTemp = Color.FromArgb(originalColor.A, 0, originalColor.G, 0)
+                    Case 2
+                        clrTemp = Color.FromArgb(originalColor.A, 0, 0, originalColor.B)
+
+                End Select
+
+
+                bmp02.SetPixel(x, y, clrTemp)
+                bmpTemp.SetPixel(x, y, clrTemp)
+            Next
+        Next
+
+        pictureDisplay.Image = bmp02
+
+    End Sub
+
+
+End Class
